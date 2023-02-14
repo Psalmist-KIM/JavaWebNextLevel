5ed0a9b747dca4a9377afd0b1d293e0bf22da2ff
Psalmist-KIM <dss02094@gmail.com>
2023-02-14 16:56:59 +0900

    This commit adds WebServer and RequestHandler

---
+++ src/main/java/chapter3/webserver/WebServer.java
@@ -0,0 +1,33 @@
+package chapter3.webserver;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        int port = 0;
+        if(args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        // 서버 소켓을 생성한다. 웹 서버는 기본적으로 8080번 포트를 사용한다.
+        try(ServerSocket listenSocket = new ServerSocket(port)) {
+            log.info("Web Application Server Started {} port.", port);
+
+            // 클라이언트가 연결될 때까지 대기한다.
+            Socket connection;
+            while((connection = listenSocket.accept()) != null) {
+                RequestHandler requestHandler = new RequestHandler(connection);
+            }
+        }
+    }
+}
